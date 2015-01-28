2983f4a7135d967183829fd81719fae1765970fc
dbouchillon <dbouchillon@zenoss.com>
2015-01-28 13:18:47 -0600

    Update preinstall.py

--- preinstall.py
+++ preinstall.py
@@ -6,7 +6,7 @@ import shutil
 def mymain(argv):
   print 'Extracting kerberos.so from {0}'.format(argv[0])
   zp_egg = zipfile.ZipFile(argv[0])
-  dest = os.path.join(os.environ['$ZENHOME'], 'lib','python','kerberos.so')
+  dest = os.path.join(os.environ['ZENHOME'], 'lib','python','kerberos.so')
   zp_egg.extract('ZenPacks/zenoss/Microsoft/Windows/lib/kerberos_el6/kerberos.so')
   zp_egg.close()
   print 'Copying kerberos.so to {0}'.format(dest)
